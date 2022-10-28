1283bb16fb50754d3b0e5199ab7a639ca922ef2c
nova chandra kirana <101784704+novachan10@users.noreply.github.com>
2022-10-28 14:12:09 +0700

    Add files via upload

---
+++ php native/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Latihan 1</title>
+  </head>
+  <body>
+  <?php
+   $nama = "Nova";
+   $umur = 16;
+   $berat_badan = 55.0;
+   $tinggi_badan = 167.0;
+   $status = true;
+
+   echo "Nama siswa = " .$nama. "<br> Umur siswa =   " .$umur. " <br> berat badan siswa = ".$tinggi_badan" <br> tinggi badan siswa = " .$berat_badan. " <br> status " .$status;
+  ?>
+  </body>
+</html>
---
+++ php native/latihan_2.php
@@ -0,0 +1,10 @@
+<?php
+    $dataProduk = [
+        //nama produk, merk, harga, status stok
+        ["Galaxy S21", "Samsung", 19000000, true],
+        ["Galaxy A71", "Samsung", 6999000, false],
+        ["iPhone 12 Pro Max", "Apple", 20999000, true],
+        ["iPhone 12 Mini", "Apple", 10999000, false],
+    ];
+    echo "$dataProduk[2]";
+?>
